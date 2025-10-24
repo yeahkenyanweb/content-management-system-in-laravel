4860608215d9d83c1e1eb0e9bd0ec89645a2ea8c
Yeah Kenyan <yeahkenyanweb@gmail.com>
2025-10-24 14:18:56 +0300

    edited

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -2,8 +2,27 @@
 @section('content')
     <div class="container-fluid mt-0 pt-0">
         @if($post->images->isNotEmpty())
-                <div id="postCarousel" class="carousel slide mb-4" data-bs-ride="carousel">
+                <div id="postCarousel" class="carousel slide mb-4" data-bs-ride="carousel" style="height: fit-content">
                     <div class="carousel-inner">
                         @foreach($post->images as $key => $image)
                             <div class="carousel-item {{ $key === 0 ? 'active' : '' }}">
-                                <img src="{{ asset('storage/' . $imag
+                                <img src="{{ asset('storage/' . $image->path) }}"
+                                     class="d-block w-100 rounded shadow-sm img-fluid h-50"
+                                     alt="Post Image">
+                            </div>
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#postCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#postCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </button>
+                </div>
+            @endif
+        <div class="card-title mt-2 text-capitalize">{{ $post->title }}</div>
+        <div class="card-text">
+            {!! $post->body !!}
+        </div>
+    </div>
+@endsection
